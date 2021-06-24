aab3c0b1aacb125c8d3e35a55d84e7165f5e991c
MrTech0 <linuxti@protonmail.com>
2021-06-24 11:30:44 +0200

    applying good practices

--- ufi.sh
+++ ufi.sh
@@ -2,27 +2,27 @@
 #
 
 function TiempoInicio {
-	tinicio=`date +%s`
+	tinicio=$(date +%s)
 }
 
 function TiempoFin {
-	tfin=`date +%s`
-	(( total = $tfin - $tinicio ))
-	(( dias = $total / ( 24 * 60 * 60) ))
-	(( rdias = $total % ( 24 * 60 * 60) ))
-	(( horas = $rdias / ( 60 * 60) ))
-	(( rhoras = $rdias % ( 60 * 60) ))
-	(( minutos = $rhoras / 60 ))
-	(( rminutos = $rhoras % 60 ))
-	if (( $dias != 0 ))
+	tfin=$(date +%s)
+	(( total = tfin - tinicio ))
+	(( dias = total / ( 24 * 60 * 60) ))
+	(( rdias = total % ( 24 * 60 * 60) ))
+	(( horas = rdias / ( 60 * 60) ))
+	(( rhoras = rdias % ( 60 * 60) ))
+	(( minutos = rhoras / 60 ))
+	(( rminutos = rhoras % 60 ))
+	if (( dias != 0 ))
 	then
 		demora="$dias días,"
 	fi
-	if (( $horas != 0 ))
+	if (( horas != 0 ))
 	then
 		demora+=" $horas horas,"
 	fi
-	if (( $minutos != 0 ))
+	if (( minutos != 0 ))
 	then
 		demora+=" $minutos minutos y"
 	fi
@@ -32,7 +32,7 @@ function TiempoFin {
 }
 
 function VerificarFedora {
-  if [ "`uname -r | grep .fc3.. | wc -l`" = 0 ] # Compruebo si el SO es Fedora 3x
+  if [ "$(uname -r | grep -c .fc3..)" = 0 ] # Compruebo si el SO es Fedora 3x
   then
     echo "Este Script es para aprovisionar una maquina Fedora, y debe correr sobre Fedora 3x"
     echo
@@ -41,9 +41,9 @@ function VerificarFedora {
 }
 
 function VerificarRoot {
-  if [ "`id -u`"  != 0 ] # Compruebo si soy root 
+  if [ "$(id -u)"  != 0 ] # Compruebo si soy root 
   then
-    echo "`whoami`: Tienes que ser root para actualizar el kernel"
+    echo "$(whoami): Tienes que ser root para actualizar el kernel"
     echo
     exit 2
   fi
@@ -51,16 +51,18 @@ function VerificarRoot {
 
 function OptimizarDNF {
 	local FICHERO="/etc/dnf/dnf.conf"
-	local existe=`grep -c fastestmirror $FICHERO`
-	if [  $existe == 0 ] # si no existe la pongo
+	local existe
+	existe=$(grep -c fastestmirror $FICHERO)
+	
+	if [  "$existe" == 0 ] # si no existe la pongo
 	then
-		echo 'fastestmirror=True' >> `echo $FICHERO`
+		echo 'fastestmirror=True' >> $FICHERO
 	fi
 
-	existe=`grep -c max_parallel_downloads $FICHERO`
-	if [  $existe == 0 ] # si no existe la pongo
+	existe=$(grep -c max_parallel_downloads $FICHERO)
+	if [  "$existe" == 0 ] # si no existe la pongo
 	then
-		echo 'max_parallel_downloads=10' >> `echo $FICHERO`
+		echo 'max_parallel_downloads=10' >> $FICHERO
 	fi
 }
 
@@ -68,7 +70,7 @@ function RepoFusion {
 
 clear
 
-echo "Actualizando sistema y instalndo repos basicos"
+echo "Actualizando sistema y instalando repos básicos"
 sleep 3
 
 dnf clean all
@@ -77,7 +79,7 @@ dnf -y update
 
 dnf -y autoremove 
 
-dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
 
 }
 
@@ -87,16 +89,20 @@ function RepoChrome {
 
 clear
 
-cd /etc/yum.repos.d/
+local FICHERO
 
-echo '[google-chrome]' > google-chrome.repo
-echo 'name=google-chrome' >> google-chrome.repo
-echo 'baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64' >> google-chrome.repo
-echo 'enabled=1' >> google-chrome.repo
-echo 'gpgcheck=1' >> google-chrome.repo
-echo 'gpgkey=https://dl.google.com/linux/linux_signing_key.pub' >> google-chrome.repo
+FICHERO="/etc/yum.repos.d/google-chrome.repo"
 
-cd /tmp
+        {
+
+		echo '[google-chrome]'
+		echo 'name=google-chrome'
+		echo 'baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64'
+		echo 'enabled=1'
+		echo 'gpgcheck=1'
+		echo 'gpgkey=https://dl.google.com/linux/linux_signing_key.pub'
+
+	} >> $FICHERO
 
 }
 
@@ -143,6 +149,7 @@ sleep 3
 dnf install libwebp webp-pixbuf-loader libopenraw libopenraw-pixbuf-loader unrar libunrar p7zip p7zip-plugins gstreamer1-plugin-openh264 h264enc mozilla-openh264 openh264 x264 x264-libs libde265 x265 x265-libs gstreamer1 gstreamer1-libav gstreamer1-plugin-openh264 gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-free-fluidsynth gstreamer1-plugins-bad-free-wildmidi gstreamer1-plugins-bad-free-zbar gstreamer1-plugins-bad-freeworld gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-good-extras gstreamer1-plugins-good-gtk gstreamer1-plugins-good-qt gstreamer1-plugins-ugly gstreamer1-plugins-ugly-free gstreamer1-vaapi gstreamer1-svt-av1 svt-av1 svt-av1-libs gstreamer1-svt-vp9 rav1e rav1e-libs dav1d libdav1d libva libva-utils vlc redhat-lsb-core kernel-devel gnome-tweaks gnome-extensions-app eclipse-jdt grub-customizer rpmfusion-free-appstream-data rpmfusion-nonfree-appstream-data -y
 
 dnf autoremove -y
+
 }
 
 function InstalarApps {
@@ -170,9 +177,13 @@ groupadd docker
 
 #Indicar nombre usuario
 
-read -p "¿Cual es el nombre de tu usuario personal?\n" usuario
+read -rp "¿Cual es el nombre de tu usuario personal? " usuario
 
-usermod -aG docker $usuario
+if [ -n "$usuario" ]
+  then
+	usermod -aG docker "$usuario"
+	echo
+fi
 
 #Limpiamos dependencias
 
@@ -189,10 +200,10 @@ systemctl enable --now docker
 
 # Aplicar cambios
 function Reiniciar {
-  read -p "¿Quieres reiniciar el sistema? [s/N]: " respuesta
-  if [ ! -z $respuesta ]
+  read -rp "¿Quieres reiniciar el sistema? [s/N]: " respuesta
+  if [ -n "$respuesta" ]
   then
-    if [ $respuesta == "s" ] || [ $respuesta == "S" ]
+    if [ "$respuesta" == "s" ] || [ "$respuesta" == "S" ]
     then 
       echo "---------------------------"
       echo "    Rebotaaaaaaandooo...   "
